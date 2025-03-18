f4570d6171b406a52c7964b3c2944597c52755c6
gustvvosilvaVMHOME <gustavosilvadeoliveira02@gmail.com>
2025-03-18 14:06:18 -0300

    spliting parallel and sequential
    english from the streets 8D

--- main.c
+++ main.c
@@ -5,18 +5,12 @@
 #include <omp.h>
 #include <stdio.h>
 
-void execution(int n_threads) {
+void execution_sequential() {
   int execs = 10;
   int array_size = 1000000;
 
-  double r_recursive_parallel = 0.0f;
   double r_recursive_sequential = 0.0f;
   double r_iterative_sequential = 0.0f;
-  double r_iterative_parallel = 0.0f;
-
-  for (int i = 0; i < execs; i++) {
-    r_recursive_parallel += execution_recursive_parallel(array_size);
-  }
 
   for (int i = 0; i < execs; i++) {
     r_recursive_sequential += execution_recursive_sequential(array_size);
@@ -26,6 +20,23 @@ void execution(int n_threads) {
     r_iterative_sequential += execution_iterative_sequential(array_size);
   }
 
+  printf("Average time taken per execution (recursive-sequential): "
+         "%f\n", r_recursive_sequential / execs);
+  printf("Average time taken per execution (iterative_sequential): "
+         "%f\n", r_iterative_sequential / execs);
+}
+
+void execution_parallel(int n_threads) {
+  int execs = 10;
+  int array_size = 1000000;
+
+  double r_recursive_parallel = 0.0f;
+  double r_iterative_parallel = 0.0f;
+
+  for (int i = 0; i < execs; i++) {
+    r_recursive_parallel += execution_recursive_parallel(array_size);
+  }
+
   for (int i = 0; i < execs; i++) {
     r_iterative_parallel += execution_iterative_parallel(array_size);
   }
@@ -33,36 +44,34 @@ void execution(int n_threads) {
   printf(
       "Average time taken per execution (recursive-parallel %d threads): %f\n",
       n_threads, r_recursive_parallel / execs);
-  printf("Average time taken per execution (recursive-sequential %d threads): "
-         "%f\n",
-         n_threads, r_recursive_sequential / execs);
   printf(
       "Average time taken per execution (iterative_parallel %d threads): %f\n",
       n_threads, r_iterative_parallel / execs);
-  printf("Average time taken per execution (iterative_sequential %d threads): "
-         "%f\n",
-         n_threads, r_iterative_sequential / execs);
 }
 
 int main() {
 
+  execution_sequential();
+
+  printf("\n");
+
   omp_set_num_threads(2);
-  execution(2);
+  execution_parallel(2);
 
   printf("\n");
 
   omp_set_num_threads(4);
-  execution(4);
+  execution_parallel(4);
 
   printf("\n");
 
   omp_set_num_threads(8);
-  execution(8);
+  execution_parallel(8);
 
   printf("\n");
 
   omp_set_num_threads(16);
-  execution(16);
+  execution_parallel(16);
 
   return 0;
 }
